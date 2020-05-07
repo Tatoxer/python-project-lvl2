05f7a83b0f2ecf326fde41da18a534f24208a528
Anton Selifanov <Tatoxer@gmail.com>
2020-05-07 13:22:20 +0300

    added FORMAT option

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,8 +2,9 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description="Generate difference")
-parser.add_argument("file_1")
-parser.add_argument("file_2")
+parser.add_argument("file_1", type=str, help='path to file_1')
+parser.add_argument("file_2", type=str, help='path to file_2')
+parser.add_argument('-f', '--FORMAT', type=str, help='set format for output file')
 args = parser.parse_args()
 print(args)
 
