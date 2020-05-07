ded24a9e41d29531fbed8e7423348137a00c6155
Anton Selifanov <Tatoxer@gmail.com>
2020-05-07 15:29:58 +0300

    modified generate_difference function

--- gendiff/generate_difference.py
+++ gendiff/generate_difference.py
@@ -1,3 +1,5 @@
+import argparse
+
 before = {
   "host": "hexlet.io",
   "timeout": 50,
@@ -11,28 +13,42 @@ after = {
 }
 
 
+def make_description():
+    parser = argparse.ArgumentParser(description="Generate difference")
+    parser.add_argument("file_1", type=str, help='path to file_1')
+    parser.add_argument("file_2", type=str, help='path to file_2')
+    parser.add_argument('-f', '--FORMAT', type=str, default=".json", help='set format for output file')
+    args = parser.parse_args()
+    print(args)
+    return args.file_1, args.file_2
+
+
+def generate_added_key(key, value):
+    formatted_key = f'+ {key}'
+    return {formatted_key: value}
+
+
+def generate_removed_key(key, value):
+    formatted_key = f'- {key}'
+    return {formatted_key: value}
+
+
 def generate_diff(file_1, file_2):
     difference = {}
     for key, value in file_1.items():
-        if key in file_2:
-            if value != file_2[key]:
-                formatted_key = f'- {key}'
-                difference[formatted_key] = file_1[key]
-
-                formatted_key = f'+ {key}'
-                difference[formatted_key] = file_2[key]
+        if key in file_2 and value != file_2[key]:
+            difference.update(generate_removed_key(key, value))
+            difference.update(generate_added_key(key, file_2[key]))
 
         if key not in file_2:
-            formatted_key = f'- {key}'
-            difference[formatted_key] = file_1[key]
+            difference.update(generate_removed_key(key, file_1[key]))
 
     for key, value in file_2.items():
         if key not in file_1:
-            formatted_key = f'+ {key}'
-            difference[formatted_key] = file_2[key]
+            difference.update(generate_added_key(key, file_2[key]))
 
     for key, value in difference.items():
-        print(key + ":" + str(value))
+        print(f'{key}: {str(value)}')
 
 
 print(generate_diff(before, after))
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python3
-import argparse
 from gendiff import generate_difference
 
 
-def print_description():
-    parser = argparse.ArgumentParser(description="Generate difference")
-    parser.add_argument("file_1", type=str, help='path to file_1')
-    parser.add_argument("file_2", type=str, help='path to file_2')
-    parser.add_argument('-f', '--FORMAT', type=str, default=".json", help='set format for output file')
-    args = parser.parse_args()
-    print(args)
-
-
 def main():
-    print_description()
-    generate_difference.generate_diff()
+    file_1, file_2 = generate_difference.make_description()
+    generate_difference.generate_diff(file_1, file_2)
 
 
 if __name__ == "__main__":
