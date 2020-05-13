8769e96169b433054705caa194b91908153ea32a
Anton Selifanov <Tatoxer@gmail.com>
2020-05-13 14:16:27 +0300

    added markers on dict keys

--- gendiff/generate_difference.py
+++ gendiff/generate_difference.py
@@ -1,72 +1,101 @@
 from collections import OrderedDict
 
 
-def generate_added_key(key, value):
-    formatted_key = f'+ {key}'
-    return {formatted_key: value}
-
-
-def generate_removed_key(key, value):
-    formatted_key = f'- {key}'
-    return {formatted_key: value}
-
-
-def print_keys_values(dictionary):
-    for key, value in dictionary.items():
-        print(f'{key}: {value}')
-
-
 def mark_difference(key, before, after):
     difference = {}
     if key in after and before[key] != after[key]:
-        difference.update(generate_removed_key(key, before[key]))
-        difference.update(generate_added_key(key, after[key]))
+        difference[key] = mark_removed(key, before, after)
+        difference[key] = mark_added(key, before, after)
     return difference
 
 
 def mark_removed(key, dictionary_before, dictionary_after):
-    difference = {}
     if key not in dictionary_after:
-        difference.update(generate_removed_key(key, dictionary_before[key]))
-    return difference
+        key = (REMOVED, dictionary_before[key])
+    return key
 
 
 def mark_added(key, dictionary_before, dictionary_after):
-    difference = {}
     if key not in dictionary_before:
-        difference.update(generate_added_key(key, dictionary_after[key]))
-    return difference
+        key = (ADDED, dictionary_after[key])
+    return key
 
 
 def mark_non_changed(key, dictionary_before, dictionary_after):
-    difference = {}
     if key in dictionary_after and dictionary_before[key] == dictionary_after[key]:
-        difference.update({key: dictionary_before[key]})
-    return difference
+        key = (NON_CHANGED, dictionary_before[key])
+    return key
 
 
-difference = OrderedDict()
+REMOVED, ADDED, NON_CHANGED, CHANGED = "removed", "added", "non_changed", "changed"
 
 
 def generate_diff(before, after):
     for key, value in before.items():
-        if isinstance(value, (dict, list)):
+        if key not in after:
+            before[key] = (REMOVED, value)
+
+        elif isinstance(value, (dict, list)):
             if key not in after:
-                difference.update(generate_removed_key(key, before[key]))
+                before[key] = (REMOVED, value)
                 continue
+
+            before[key] = (NON_CHANGED, value)
             generate_diff(value, after[key])
+
+        elif key not in after:
+            before[key] = (REMOVED, before[key])
+        elif key in after and value != after[key]:
+            before[key] = (CHANGED, value, after[key])
         else:
-            difference.update(mark_difference(key, before, after))
-            difference.update(mark_non_changed(key, before, after))
+            before[key] = (NON_CHANGED, value)
 
     for key, value in after.items():
         if isinstance(value, (dict, list)):
             if key not in before:
-                difference.update(mark_added(key, before, after))
+                before[key] = (ADDED, value)
                 continue
+        before[key] = (ADDED, value)
 
-    return difference
+    return before
 
+
+test1 = {
+  "common": {
+    "setting1": "Value 1",
+    "setting2": "200",
+    "setting3": "true",
+    "setting6": {
+      "key": "value"
+    }
+  },
+  "group1": {
+    "baz": "bas",
+    "foo": "bar"
+  },
+  "group2": {
+    "abc": "12345"
+  }
+}
+test2 = {
+  "common": {
+    "setting1": "Value 1",
+    "setting3": "true",
+    "setting4": "blah blah",
+    "setting5": {
+      "key5": "value5"
+    }
+  },
+
+  "group1": {
+    "foo": "bar",
+    "baz": "bars"
+  },
+
+  "group3": {
+    "fee": "100500"
+  }
+}
 
 
 
