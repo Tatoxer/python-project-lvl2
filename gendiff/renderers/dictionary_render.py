66b06a3f5436691ccf1a9a3fa503d2b4b76c2c19
Anton Selifanov <Tatoxer@gmail.com>
2020-05-27 12:50:45 +0300

    added working colors

--- gendiff/renderers/dictionary_render.py
+++ gendiff/renderers/dictionary_render.py
@@ -2,14 +2,6 @@ from colorama import Fore
 from gendiff.difference import REMOVED, ADDED, NON_CHANGED, CHANGED, NESTED, generate_diff
 from gendiff.files import read_file
 
-MARKERS = {
-    "non_changed": None,
-    "removed": "- ",
-    "added": "+ ",
-    "changed": None,
-    "nested": None
-}
-
 
 def make_result(list_, spaces):
     string_spaces = " " * spaces
@@ -17,21 +9,25 @@ def make_result(list_, spaces):
 
     for (key, status, value) in list_:
         if status == NESTED:
-            result.append(f"{string_spaces} {key}:{value}")
+            result.append(Fore.WHITE + f"{string_spaces} {key}: {value}\n")
 
         elif status == REMOVED:
-            result.append(f"{string_spaces}- {key}: {value}\n")
+            result.append(Fore.RED + f"{string_spaces}- {key}: {value}\n")
+            result.append(Fore.WHITE + "")
 
         elif status == ADDED:
-            result.append(f"{string_spaces}+ {key}: {value}\n")
+            result.append(Fore.GREEN + f"{string_spaces}+ {key}: {value}\n")
+            result.append(Fore.WHITE + "")
 
         elif status == CHANGED:
-            result.append(f"{string_spaces}- {key}: {value[0]}\n")
-            result.append(f"{string_spaces}+ {key}: {value[1]}\n")
+            result.append(Fore.RED + f"{string_spaces}- {key}: {value[0]}\n")
+            result.append(Fore.GREEN + f"{string_spaces}+ {key}: {value[1]}\n")
+            result.append(Fore.WHITE + "")
 
-        elif status == NON_CHANGED:
+        else:
             result.append(f"{string_spaces}  {key}: {value}\n")
-    result.append("}\n")
+
+    result.append(' ' * (spaces - 2) + '}' + "\n")
     return result
 
 
@@ -44,21 +40,12 @@ def render_dictionary(dictionary, spaces=2):
             strings.append((key, status, value))
 
     result = (make_result(strings, spaces))
+    #"".join(result)
     return "".join(result)
-
-
-# def check_color_condition(index, string, result, color):
-#     if "{" in string[index]:
-#         while "}" not in string[index]:
-#             result += color + string[index] + "\n"
-#             index += 1
-#     return result, index
 
 
 file1 = read_file("/home/tatoxa/python_projects/python-project-lvl2/tests/fixtures/test_before_2.json")
 file2 = read_file("/home/tatoxa/python_projects/python-project-lvl2/tests/fixtures/test_after_2.json")
 d = (generate_diff(file1, file2))
 print(render_dictionary(d))
-# s = render_dictionary(d)
-# for elem in s:
-#     print(elem, type(elem))
+
