47153546088156b6d23a3a0c8f15c1a2bc82345a
Anton Selifanov <Tatoxer@gmail.com>
2020-05-27 17:48:25 +0300

    refactoring difference

--- gendiff/difference.py
+++ gendiff/difference.py
@@ -1,3 +1,5 @@
+from gendiff.files import read_file
+
 REMOVED, ADDED, NON_CHANGED, CHANGED, NESTED = (
     "removed", "added", "non_changed", "changed", "nested"
 )
@@ -5,7 +7,7 @@ REMOVED, ADDED, NON_CHANGED, CHANGED, NESTED = (
 
 def mark_keys(dictionary1, dictionary2):
     result = {
-        NON_CHANGED: (dictionary1.keys() & dictionary2.keys()) | dictionary1.keys() & dictionary1.keys(),
+        NON_CHANGED: dictionary1.keys() & dictionary2.keys(),
         REMOVED: dictionary1.keys() - dictionary2.keys(),
         ADDED: dictionary2.keys() - dictionary1.keys()}
     return result
@@ -13,31 +15,25 @@ def mark_keys(dictionary1, dictionary2):
 
 def generate_diff(before, after):
     key_status = mark_keys(before, after)
-
-    for key, value in before.items():
-        if isinstance(value, dict) and key in key_status[NON_CHANGED]:
-            before[key] = (NESTED, value)
-            generate_diff(value, after[key])
-
-        elif key in key_status[REMOVED]:
-            if isinstance(value, dict):
-                before[key] = (REMOVED, value)
-                generate_diff(value, value)
-            else:
-                before[key] = (REMOVED, value)
-
-        elif key in after and not value == after[key]:
-            before[key] = (CHANGED, (value, after[key]))
-
+    result = {}
+    for key in key_status[NON_CHANGED]:
+        if before[key] == after[key]:
+            result[key] = (NON_CHANGED, before[key])
         else:
-            before[key] = (NON_CHANGED, value)
-
-    for key, value in after.items():
-        if key in key_status[ADDED]:
-            if isinstance(after[key], dict):
-                before[key] = (ADDED, value)
-                generate_diff(value, value)
+            if isinstance(before[key], dict) and isinstance(after[key], dict):
+                result[key] = (NESTED, generate_diff(before[key], after[key]))
             else:
-                before[key] = (ADDED, value)
+                result[key] = (CHANGED, (before[key], after[key]))
 
-    return before
+    for key in key_status[REMOVED]:
+        result[key] = (REMOVED, before[key])
+
+    for key in key_status[ADDED]:
+        result[key] = (ADDED, after[key])
+
+    return result
+
+
+file1 = read_file("/home/tatoxa/python_projects/python-project-lvl2/tests/fixtures/test_before_2.json")
+file2 = read_file("/home/tatoxa/python_projects/python-project-lvl2/tests/fixtures/test_after_2.json")
+# print(generate_diff(file1, file2))
